df7cc35db4850d693aa1257d03c7039c7e2d055d
me19 <me19@pomscloud.ie>
2020-09-18 14:58:33 +0200

    Image

---
+++ README.md
@@ -0,0 +1,3 @@
+# htmlimage-puppeteer
+
+Call ```node htmlimage.js url=google.com width=300 height=250 output=image.jpg type=jpeg delay=1500 timeoutInSeconds=120 fullPage=false``` in cli and take screenshot with puppeteer.
---
+++ htmlimage.js
@@ -0,0 +1,108 @@
+const puppeteer = require('puppeteer');
+
+const parameters = {
+    url: 'https://google.com',
+    output: 'screenshot.jpg',
+    width: 300,
+    height: 250,
+    type: 'jpeg',
+    quality: 50,
+    delay: 1500,
+    fullPage: false,
+
+    // 2 minutes to timeout
+    timeoutInSeconds: 60
+};
+
+
+
+// Define sleep function
+const sleep = m => new Promise(r => setTimeout(r, m));
+
+// Get parameters
+process.argv.forEach(function (val, index) {
+    const argument = val.split('=');
+    parameters[argument[0]] = argument[1];
+});
+
+(async () => {
+    try {
+        process.stdout.write('Screenshot generating started from: ' + parameters.url + '\n');
+
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        // Go to url
+        await page.goto(parameters.url.replace('\\', '/'));
+
+        // Set screen width and height
+        await page.setViewport({
+            width: parseInt(parameters.width),
+            height: parseInt(parameters.height),
+        });
+
+        // Take screenshot after delay
+        await sleep(parameters.delay);
+
+        // Promise for timeout
+        const timeoutPromise = new Promise(async (resolve, reject) => {
+            setTimeout(async () => {
+
+                // Send reject
+                reject('Html to image generating timed out' + '\n');
+
+            }, parameters.timeoutInSeconds * 1000);
+        });
+
+        // Promise for screenshot
+        const screenshotPromise = new Promise(async (resolve, reject) => {
+
+            await page.screenshot({
+                path: parameters.output,
+                type: parameters.type,
+                quality: parameters.quality,
+                fullPage: parameters.fullPage
+            });
+
+            // Send resolve
+            resolve();
+
+        });
+
+        // Race with screenshot vs timeout
+        await Promise.race([screenshotPromise, timeoutPromise]).then(async () => {
+
+            process.stdout.write('Html to image generated successfully to: ' + parameters.output + '\n');
+
+            // Close the browser and kill process
+            if (typeof browser !== 'undefined') {
+                await browser.close();
+            }
+
+            await process.kill(process.pid);
+
+        }).catch(async (error) => {
+
+            process.stdout.write('Failed to generate image from html: ' + error + '\n');
+
+            // Close the browser and kill process
+            if (typeof browser !== 'undefined') {
+                await browser.close();
+            }
+
+            await process.kill(process.pid);
+
+        });
+    } catch (error) {
+
+        process.stdout.write('Html to image generating failed with: ' + error + '\n');
+
+        // Close the browser and kill process
+        if (typeof browser !== 'undefined') {
+            await browser.close();
+        }
+
+        await process.kill(process.pid);
+
+    }
+})();
